a1bfe8515389ab61540d75d189f4ef7ba79e36b5
janne hjuler jensen <jann778j@stud.kea.dk>
2022-11-11 10:12:12 +0100

    added member to finnished team

--- src/components/FinishedTeam.jsx
+++ src/components/FinishedTeam.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Member from './Member'
+
 
 function FinishedTeam(props) {
   return (
-    <div>FinishedTeamn {props.team.length}</div>
+    <div className='FinishedTeam'>
+        {/* FinishedTeamn: {props.team.length} */}
+    
+    {props.team.map((membernew)=>(<Member key={membernew.id} data={membernew} fullView={true} {...membernew} />))}
+    </div>
+    
   )
 }
 
