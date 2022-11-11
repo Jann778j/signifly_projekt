0d78b68bfff92c73914c02dd994b04f4b717377c
janne hjuler jensen <jann778j@stud.kea.dk>
2022-11-11 11:32:49 +0100

    added link to phone

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -7,13 +7,17 @@ function Member(props) {
     }
   return (
        <article className='Member'>
+        {/* {props.fullView === true &&  <img src={props.data.imgfull} alt="person" />} */}
+        {/* {props.fullView === false &&  <img src={props.data.img} alt="person" />} */}
         <img src={props.data.img} alt="person" />
+       <div>
         <h2>{props.data.name}</h2>
             <h3>{props.data.stilling}</h3>
        {props.fullView === true && <h4><a href={`mailto:${props.data.email}`}>{props.data.email}</a></h4>} 
-    {props.fullView === true && <p>{props.data.phone}</p>} 
+    {props.fullView === true && <p><a href={`tel:${props.data.phone}`}>{props.data.phone}</a></p>} 
          {props.fullView === true && <p>{props.data.beskrivelse}</p>} 
         {props.fullView === false && <button onClick={add}>ADD TO TEAM</button>}
+        </div>
       
 
     </article>
