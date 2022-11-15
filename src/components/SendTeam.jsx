0e924083fcf3c37a4c0ae44541a150723f1b85d8
janne hjuler jensen <jann778j@stud.kea.dk>
2022-11-15 08:36:05 +0100

    added client view

--- src/components/SendTeam.jsx
+++ src/components/SendTeam.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import {useState} from 'react'
 
 function SendTeam(props) {
+    const [sent, setSent] = useState(false)
+    if(sent){
+        return <p>Team sendt!</p>
+    }
   return (
     <div className='SendTeam'>
-<p>Client E-mai:</p>
-<input className='message' type="email" required />
-<br />
-<br />
-<br />
-<p>Message:</p>
-<textarea className='message' name="message" id="message" cols="30" rows="4"></textarea>
-<button >SEND TEAM</button>
 
- <button onClick={()=>props.setShowTeam(false)}>BACK</button>
+         <button onClick={()=>props.setShowTeam(false)}>BACK</button>
+         <br />
+         <br />
+         <br />
+         <br />
+         <form action="">
+            <label htmlFor="form-email">Client E-mai:</label>
+<input className='message' required type="email"  name="email" id="form-email" />
+<br />
+<br />
+<br />
+<label htmlFor="form-message">Message:</label>
+<textarea className='message' name="message" id="message" cols="30" rows="4"></textarea>
+<button onClick={()=>setSent(true)}>SEND TEAM</button>
+</form>
+
+
     </div>
 
   )
