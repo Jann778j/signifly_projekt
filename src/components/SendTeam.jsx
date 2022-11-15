3ac8adbf43e01cd6ec050b23205c5d71f7f940f6
janne hjuler jensen <jann778j@stud.kea.dk>
2022-11-15 15:50:46 +0100

    color fix

--- src/components/SendTeam.jsx
+++ src/components/SendTeam.jsx
@@ -9,20 +9,20 @@ function SendTeam(props) {
     <div className='SendTeam'>
 
          <button onClick={()=>props.setShowTeam(false)}>BACK</button>
-         <br />
-         <br />
-         <br />
-         <br />
+ <br />
+ <br />
+ <br />
+ <br />
          <form action="">
             <label htmlFor="form-email">Client E-mai:</label>
-<input className='message' required type="email"  name="email" id="form-email" />
+              <input className='message' required type="email"  name="email" id="form-email" />
 <br />
 <br />
 <br />
-<label htmlFor="form-message">Message:</label>
-<textarea className='message' name="message" id="message" cols="30" rows="4"></textarea>
-<button onClick={()=>setSent(true)}>SEND TEAM</button>
-</form>
+            <label htmlFor="form-message">Message:</label>
+              <textarea className='message' name="message" id="message" cols="30" rows="4"></textarea>   
+            <button onClick={()=>setSent(true)}>SEND TEAM</button>
+          </form>
 
 
     </div>
